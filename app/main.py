f833313a7af8f8e87a2ffc52049ebc984a2b5225
mahimairaja <mahimairaja3@gmail.com>
2025-07-20 15:07:39 -0400

    feat: Added ruff for linting and simple post - get/post request enpoints with pydantic models

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
+from typing import List
+
 from fastapi import FastAPI
 
+from app.models.post import UserPost, UserPostIn
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World!"}
 
+post_table = {}
+
+
+@app.post("/post", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.model_dump()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+
+
+@app.get("/post", response_model=List[UserPost])
+async def get_all_posts():
+    return list(post_table.values())
