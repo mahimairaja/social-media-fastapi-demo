8adee59753de383803b0569f2d2e29dd83e4e99a
mahimairaja <mahimairaja3@gmail.com>
2025-07-20 15:10:59 -0400

    feat: refactored to routers format

---
+++ app/routers/post.py
@@ -0,0 +1,24 @@
+from typing import List
+
+from fastapi import APIRouter
+
+from app.models.post import UserPost, UserPostIn
+
+router = APIRouter()
+
+
+post_table = {}
+
+
+@router.post("/post", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.model_dump()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+
+
+@router.get("/post", response_model=List[UserPost])
+async def get_all_posts():
+    return list(post_table.values())
