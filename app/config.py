56bf77f28d4c839afedfc3aa068377e928faadfe
mahimairaja <mahimairaja3@gmail.com>
2025-07-24 17:15:03 -0400

    feat: loading environmental variables using pydantic settings

---
+++ app/config.py
@@ -0,0 +1,40 @@
+from functools import lru_cache
+from typing import Optional
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class BaseConfig(BaseSettings):
+    ENV_STATE: Optional[str] = None
+
+    class Config:
+        env_file = ".env"
+
+
+class GlobalConfig(BaseConfig):
+    DATABASE_URL: Optional[str] = None
+    DB_FORCE_ROLL_BACK: bool = False
+
+
+class DevConfig(GlobalConfig):
+    model_config = SettingsConfigDict(env_prefix="DEV_")
+
+
+class ProdConfig(GlobalConfig):
+    model_config = SettingsConfigDict(env_prefix="PROD_")
+
+
+class TestConfig(GlobalConfig):
+    model_config = SettingsConfigDict(env_prefix="TEST_")
+
+    DB_FORCE_ROLL_BACK: bool = True
+    DATABASE_URL: str = "sqlite:///test.db"
+
+
+@lru_cache()
+def get_config(env_state: str) -> GlobalConfig:
+    configs = {"dev": DevConfig, "prod": ProdConfig, "test": TestConfig}
+    return configs[env_state]()
+
+
+config = get_config(BaseConfig().ENV_STATE)
