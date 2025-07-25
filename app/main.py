96efd323ca72a8fa0ad23812157d8cf4ec2ff167
mahimairaja <mahimairaja3@gmail.com>
2025-07-25 07:12:58 -0400

    feat: added base logger configurations

--- app/main.py
+++ app/main.py
@@ -3,11 +3,13 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.database import database
+from app.logging_conf import configure_logging
 from app.routers.post import router as post_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    configure_logging()
     await database.connect()  # setup
     yield
     await database.disconnect()  # teardown
