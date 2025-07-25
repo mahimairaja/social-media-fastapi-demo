87c678b3a2e1eea642e0a0303187ef2e8348ba0c
mahimairaja <mahimairaja3@gmail.com>
2025-07-25 07:34:46 -0400

    feat: added logic to log to files and rotate on count basis

--- app/logging_conf.py
+++ app/logging_conf.py
@@ -13,19 +13,36 @@ def configure_logging() -> None:
                     "class": "logging.Formatter",
                     "datefmt": "%Y-%m-%dT%H:%M:%S",
                     "format": "%(name)s:%(lineno)d - %(message)s",
-                }
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(asctime)s:%(msecs)03dz | %(levelname)-8s | %(name)s:%(lineno)s - %(message)s",
+                },
+                # 03d 3 digit
+                # z iso format
+                # -8s always 8 characters long
             },
             "handlers": {
                 "default": {
                     "class": "rich.logging.RichHandler",
                     "level": "DEBUG",
                     "formatter": "console",
-                }
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "storeapi.log",
+                    "maxBytes": 1024 * 1025 * 2,  # 2 megabytes
+                    "backupCount": 5,
+                    "encoding": "utf8",
+                },
             },
             "loggers": {
-                "uvicorn": {"handlers": ["default"], "level": "INFO"},
+                "uvicorn": {"handlers": ["default", "rotating_file"], "level": "INFO"},
                 "app": {  # root.storeapi.routers.post
-                    "handlers": ["default"],
+                    "handlers": ["default", "rotating_file"],
                     "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
                     "propagate": False,
                 },
