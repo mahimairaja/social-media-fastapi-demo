e05f917eb3e59bb5382c1b2900542104ecf14b62
mahimairaja <mahimairaja3@gmail.com>
2025-07-28 13:32:43 -0400

    feat: updated authentication layer using dependency injection

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,7 +1,7 @@
 import logging
-from typing import List
+from typing import Annotated, List
 
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, Depends, HTTPException
 
 from app.database import comment_table, database, post_table
 from app.models.post import (
@@ -12,7 +12,7 @@ from app.models.post import (
     UserPostWithComments,
 )
 from app.models.user import User
-from app.security import get_current_user, oauth2_scheme
+from app.security import get_current_user
 
 router = APIRouter()
 
@@ -30,9 +30,10 @@ async def find_post(post_id: int):
 
 
 @router.post("/post", response_model=UserPost, status_code=201)
-async def create_post(post: UserPostIn, request: Request):
+async def create_post(
+    post: UserPostIn, current_user: Annotated[User, Depends(get_current_user)]
+):
     logger.info("Creating post")
-    current_user: User = await get_current_user(await oauth2_scheme(request))  # noqa
 
     data = post.model_dump()
     query = post_table.insert().values(**data)
@@ -52,9 +53,10 @@ async def get_all_posts():
 
 
 @router.post("/comment", response_model=Comment, status_code=201)
-async def create_comment(comment: CommentIn, request: Request):
+async def create_comment(
+    comment: CommentIn, current_user: Annotated[User, Depends(get_current_user)]
+):
     logger.info("Creating comment")
-    current_user: User = await get_current_user(await oauth2_scheme(request))  # noqa
 
     post = await find_post(comment.post_id)
     if not post:
